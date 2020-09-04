6262d64123d5c57a44bac0cfa51da718c8f9977a
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-04 11:32:47 +0530

    Initial Commit

---
+++ Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Subhrajyoti Chakraborty on 04/09/20.
+//  Copyright © 2020 Subhrajyoti Chakraborty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
